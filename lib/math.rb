87a16c9bbf6d47b05500f646b33ded4d9ab33cf2
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-27 06:18:12 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+num1 + num2
 end
+print addition(5,4)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
+print subtraction(10,5)
 
 def division(num1, num2)
-
+num1 / num2
 end
+print division(50,2)
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
+print multiplication(4,30)
 
 def modulo(num1, num2)
-
+num1 % num2
 end
+print modulo(34,5)
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+print Math.sqrt(81)
